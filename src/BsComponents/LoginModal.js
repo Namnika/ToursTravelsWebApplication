48280683539167103755044a8e83bf00b65cf2ec
Namnika <janbandhunamnika@gmail.com>
2024-10-31 12:48:29 +0530

    modify class to className

--- src/BsComponents/LoginModal.js
+++ src/BsComponents/LoginModal.js
@@ -4,44 +4,44 @@ function LoginModal() {
     return (
         <>
             <div
-                class="modal fade"
+                className="modal fade"
                 id="loginmodal"
-                tabindex="5"
+                tabIndex="5"
                 aria-labelledby="loginmodallabel"
             >
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
                             <h2 id="loginmodallabel">Login</h2>
                             <button
                                 type="button"
-                                class="btn-close"
+                                className="btn-close"
                                 data-bs-dismiss="modal"
                                 aria-label="Close"
                             ></button>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body">
                             <form>
-                                <div class="mb-2">
-                                    <label for="fullname" class="col-form-label">
+                                <div className="mb-2">
+                                    <label htmlFor="fullname" className="col-form-label">
                                         Email ID:
                                     </label>
                                     <input
                                         type="text"
-                                        class="form-control form-text"
+                                        className="form-control form-text"
                                         id="fullname"
                                     />
                                 </div>
-                                <div class="mb-2">
-                                    <label for="password" class="col-form-label">
+                                <div className="mb-2">
+                                    <label htmlFor="password" className="col-form-label">
                                         Password:
                                     </label>
-                                    <input type="tel" class="form-control" id="password" />
+                                    <input type="tel" className="form-control" id="password" />
                                 </div>
                             </form>
                         </div>
-                        <div class="modal-footer d-flex flex-column align-items-start justify-content-start gap-4">
-                            <button type="button" class="w-100 btn btn-outline-dark">
+                        <div className="modal-footer d-flex flex-column align-items-start justify-content-start gap-4">
+                            <button type="button" className="w-100 btn btn-outline-dark">
                                 Login
                             </button>
                             <p>
@@ -50,7 +50,7 @@ function LoginModal() {
                                     href="#register"
                                     data-bs-toggle="modal"
                                     data-bs-target="#registermodal"
-                                    class="text-dark"
+                                    className="text-dark"
                                 >
                                     Register
                                 </a>{" "}
--- src/BsComponents/RegisterModal.js
+++ src/BsComponents/RegisterModal.js
@@ -1,131 +1,224 @@
-import React from "react";
+import React, { useState } from "react";
 
 function RegisterModal() {
-    return (
-        <>
-            <div
-                class="modal fade"
-                id="registermodal"
-                tabindex="5"
-                aria-labelledby="registermodallabel"
-            >
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h2 id="registermodallabel">Register</h2>
-                            <button
-                                type="button"
-                                class="btn-close"
-                                data-bs-dismiss="modal"
-                                aria-label="Close"
-                            ></button>
-                        </div>
-                        <div class="modal-body">
-                            <form>
-                                <div class="mb-2">
-                                    <label for="fullname" class="col-form-label">
-                                        Full Name:
-                                    </label>
-                                    <input
-                                        type="text"
-                                        placeholder="ex. John Doe"
-                                        class="form-control form-text"
-                                        id="fullname"
-                                    />
-                                </div>
-                                <div class="mb-2">
-                                    <label for="contact" class="col-form-label">
-                                        Contact:
-                                    </label>
-                                    <input
-                                        type="tel"
-                                        placeholder="ex. 123456789"
-                                        class="form-control"
-                                        id="contact"
-                                    />
-                                </div>
-                                <div class="mb-2">
-                                    <label for="dob" class="col-form-label">
-                                        Date Of Birth:
-                                    </label>
-                                    <input
-                                        type="date"
-                                        placeholder="ex. 12/15/1995"
-                                        class="form-control"
-                                        id="dob"
-                                    />
-                                </div>
-                                <div class="mb-2">
-                                    <label for="email" class="col-form-label">
-                                        Email ID:
-                                    </label>
-                                    <input
-                                        type="email"
-                                        placeholder="ex. example@gmail.com"
-                                        class="form-control"
-                                        id="email"
-                                    />
-                                </div>
-                                <div class="mb-2">
-                                    <label for="password" class="col-form-label">
-                                        Password:
-                                    </label>
-                                    <input type="password" class="form-control" id="password" />
-                                    <small id="validation" class="text-danger fw-semibold">
-                                        Password should be minimum 8 and maximum 15 characters
-                                        contains smallcase, uppercase, one number, one special
-                                        character
-                                    </small>
-                                </div>
-                                <div class="mb-2">
-                                    <label for="gender" class="col-form-label">
-                                        Gender:
-                                    </label>
-                                    <div class="ms-4 form-check form-check-inline">
-                                        <input
-                                            type="radio"
-                                            class="form-check-input"
-                                            name="gender"
-                                            id="gender"
-                                            aria-label="female"
-                                        />
-                                        Female
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input
-                                            type="radio"
-                                            class="form-check-input"
-                                            name="gender"
-                                            id="gender"
-                                            aria-label="male"
-                                        />
-                                        Male
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="modal-footer d-flex flex-column align-items-start justify-content-start gap-4">
-                            <button type="button" class="w-100 btn btn-outline-dark">
-                                Register
-                            </button>
-                            <p>
-                                Already have an account? Go{" "}
-                                <a
-                                    href="#login"
-                                    data-bs-toggle="modal"
-                                    data-bs-target="#loginmodal"
-                                    class="text-dark"
-                                >
-                                    Login
-                                </a>{" "}
-                                Yourself!
-                            </p>
-                        </div>
-                    </div>
-                </div>
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    contact: "",
+  });
+  let notValidPass =
+    "Password should be minimum 8 and maximum 15 characters contains smallcase, uppercase, one number, one special character";
+
+  let notValidEmail =
+    "Please enter a valid email address in the format 'example@domain.com'.";
+
+  let notValidContact = "Contact Number should contain only 10 digits.";
+
+  const [notValidPassText, setValidPassText] = useState({
+    text: notValidPass,
+    className: "danger",
+  });
+  const [notValidEmailText, setValidEmailText] = useState({
+    text: notValidEmail,
+    className: "danger",
+  });
+  const [notValidContactText, setValidContactText] = useState({
+    text: notValidContact,
+    className: "danger",
+  });
+
+  const emailregex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,})$/;
+  const contactregex = /^[0-9]{3}?[0-9]{3}?[0-9]{4}$/;
+  const passregex =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*"'-]).{8,15}$/;
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+
+    if (name === "email" && emailregex.test(value)) {
+      setValidEmailText({ text: "✅", className: "success" });
+    } else {
+      setValidEmailText({
+        text: notValidEmailText.text,
+        className: "danger",
+      });
+    }
+
+    if (name === "password" && passregex.test(value)) {
+      setValidPassText({ text: "Validated", className: "success" });
+    } else {
+      setValidPassText({
+        text: "Not Validated",
+        className: "danger",
+      });
+    }
+
+    if (name === "contact" && contactregex.test(value)) {
+      setValidContactText({ text: "✅", className: "success" });
+    } else {
+      setValidContactText({
+        text: notValidContactText.text,
+        className: "danger",
+      });
+    }
+  }
+
+  return (
+    <>
+      <div
+        className="modal fade"
+        id="registermodal"
+        tabIndex="5"
+        aria-labelledby="registermodallabel"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h2 id="registermodallabel">Register</h2>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
-        </>
-    );
+            <div className="modal-body">
+              <form>
+                <div className="mb-2">
+                  <label htmlFor="fullname" className="col-form-label">
+                    Full Name:
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="ex. John Doe"
+                    className="form-control form-text"
+                    id="fullname"
+                  />
+                </div>
+                <div className="mb-2">
+                  <label htmlFor="contact" className="col-form-label">
+                    Contact:
+                  </label>
+                  <input
+                    type="tel"
+                    name="contact"
+                    value={data.contact}
+                    onChange={handleChange}
+                    placeholder="ex. 123456789"
+                    className="form-control"
+                    id="contact"
+                  />
+                  <small
+                    id="validation"
+                    className={`text-${notValidContactText.className} fw-semibold`}
+                  >
+                    {notValidContactText.text}
+                  </small>
+                </div>
+                <div className="mb-2">
+                  <label htmlFor="dob" className="col-form-label">
+                    Date Of Birth:
+                  </label>
+                  <input
+                    type="date"
+                    placeholder="ex. 12/15/1995"
+                    className="form-control"
+                    id="dob"
+                  />
+                </div>
+                <div className="mb-2">
+                  <label htmlFor="email" className="col-form-label">
+                    Email ID:
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={data.email}
+                    onChange={handleChange}
+                    placeholder="ex. example@gmail.com"
+                    className="form-control"
+                    id="email"
+                  />
+                  <small
+                    id="validation"
+                    className={`text-${notValidEmailText.className} fw-semibold`}
+                  >
+                    {notValidEmailText.text}
+                  </small>
+                </div>
+                <div className="mb-2">
+                  <label htmlFor="password" className="col-form-label">
+                    Password:
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    value={data.password}
+                    onChange={handleChange}
+                    className="form-control"
+                    id="password"
+                  />
+                  <small
+                    id="validation"
+                    className={`text-${notValidPassText.className} fw-semibold`}
+                  >
+                    {notValidPassText.text}
+                  </small>
+                </div>
+                <div className="mb-2">
+                  <label htmlFor="gender" className="col-form-label">
+                    Gender:
+                  </label>
+                  <div className="ms-4 form-check form-check-inline">
+                    <input
+                      type="radio"
+                      className="form-check-input"
+                      name="gender"
+                      id="gender"
+                      aria-label="female"
+                    />
+                    Female
+                  </div>
+                  <div className="form-check form-check-inline">
+                    <input
+                      type="radio"
+                      className="form-check-input"
+                      name="gender"
+                      id="gender"
+                      aria-label="male"
+                    />
+                    Male
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer d-flex flex-column align-items-start justify-content-start gap-4">
+              <button type="button" className="w-100 btn btn-outline-dark">
+                Register
+              </button>
+              <p>
+                Already have an account? Go{" "}
+                <a
+                  href="#login"
+                  data-bs-toggle="modal"
+                  data-bs-target="#loginmodal"
+                  className="text-dark"
+                >
+                  Login
+                </a>{" "}
+                Yourself!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default RegisterModal;
