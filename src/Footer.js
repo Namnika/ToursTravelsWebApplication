2b118a09e3496a6c07bc9724a652664cbf4ee967
Namnika <janbandhunamnika@gmail.com>
2024-10-31 12:55:21 +0530

    Add dynamic submenu to navigation links

--- src/Footer.js
+++ src/Footer.js
@@ -1,34 +1,59 @@
 import React from "react";
 
-function Footer() {
+export const FooterLinks = [
+    {
+        id: 1,
+        footerLink: "x",
+        footerIcon: <i className="fa-brands fa-x-twitter"></i>,
+    },
+    {
+        id: 2,
+        footerLink: "facebook",
+        footerIcon: <i className="fa-brands fa-facebook"></i>,
+    },
+    {
+        id: 3,
+        footerLink: "instagram",
+        footerIcon: <i className="fa-brands fa-instagram"></i>,
+    },
+    {
+        id: 4,
+        footerLink: "youtube",
+        footerIcon: <i className="fa-brands fa-youtube"></i>,
+    },
+    {
+        id: 5,
+        footerLink: "pinterest",
+        footerIcon: <i className="fa-brands fa-pinterest"></i>,
+    },
+];
+
+function Footer({ Logo }) {
     return (
         <>
-            <footer class="bg-black bg-opacity-10 d-flex flex-column align-items-center justify-content-center px-5 mt-5 pb-5 pt-2">
+            <footer className="bg-black bg-opacity-10 d-flex flex-column align-items-center justify-content-center px-5 mt-5 pb-5 pt-2">
                 {/* navbar logo  */}
-                <a href="#logo" class="py-4">
-                    <img src="images/brand-logo.png" width="100" height="30" alt="logo" />
+                <a href="#logo" className="py-4">
+                    <img src={Logo} width="100" height="30" alt="logo" />
                 </a>
-                <p class="text-muted">
+                <p className="text-muted">
                     Discover the world with us, where every journey is crafted with care
                     and passion. From unforgettable destinations to seamless travel
                     experiences, we turn your wanderlust into reality!
                 </p>
-                <ul class="d-flex gap-3">
-                    <a href="#x" class="text-black">
-                        <i class="fa-brands fa-x-twitter"></i>
-                    </a>
-                    <a href="#facebook" class="text-black">
-                        <i class="fa-brands fa-facebook"></i>
-                    </a>
-                    <a href="#instagram" class="text-black">
-                        <i class="fa-brands fa-instagram"></i>
-                    </a>
-                    <a href="#youtube" class="text-black">
-                        <i class="fa-brands fa-youtube"></i>
-                    </a>
-                    <a href="#pinterest" class="text-black">
-                        <i class="fa-brands fa-pinterest"></i>
-                    </a>
+                <ul className="d-flex gap-3">
+                    {FooterLinks.map((footer, index) => {
+                        return (
+                            <a
+                                key={index}
+                                id={footer.id}
+                                href={`#${footer.footerLink}`}
+                                className="text-black"
+                            >
+                                {footer.footerIcon}
+                            </a>
+                      );
+                  })}
                 </ul>
                 <p>Designed By Namnika Janbandhu</p>
                 <small>&copy; Copyright | All Rights Reserved</small>
--- src/Header.js
+++ src/Header.js
@@ -1,97 +1,121 @@
 import React from "react";
 
-function Header() {
-    return (
-        <>
-            <header className="px-5 py-5">
-                {/* navigation */}
-                <nav className="navbar navbar-expand-lg bg-transparent">
-                    <div className="container-fluid">
-                        {/* navbar logo  */}
-                        <a href="#logo">
-                            <img
-                                src="images/brand-logo.png"
-                                width="100"
-                                height="30"
-                                alt="logo"
-                            />
-                        </a>
+export const NavLinks = [
+  { id: 1, navTitle: "Home" },
+  { id: 2, navTitle: "Book" },
+  {
+    id: 3,
+    navTitle: {
+      title: "Packages",
+      submenu: [
+        { id: "United State", title: "United State" },
+        { id: "India", title: "India" },
+        { id: "France", title: "France" },
+        { id: "Germany", title: "Germany" },
+      ],
+    },
+  },
+  { id: 4, navTitle: "Services" },
+  { id: 5, navTitle: "Gallery" },
+  { id: 6, navTitle: "About" },
+];
 
-                        {/* navigation menu using Bootstrap */}
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#navbarNav"
-                            aria-controls="navbarNav"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                        >
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
+function Header({ Logo }) {
+  return (
+    <>
+      <header className="px-5 py-5">
+        {/* navigation */}
+        <nav className="navbar navbar-expand-lg bg-transparent">
+          <div className="container-fluid">
+            {/* navbar logo  */}
+            <a href="#logo">
+              <img src={Logo} width="100" height="30" alt="logo" />
+            </a>
 
-                        <div
-                            className="navigation collapse navbar-collapse bg-transparent"
-                            id="navbarNav"
-                        >
-                            <ul className="fw-normal px-0 mb-0 navbar-nav">
-                                <li>Home</li>
-                                <li>Book</li>
-                                <li className="hovermenu">
-                                    <span>Packages</span>
-                                    <div className="hovermenuitem">
-                                        <p>United State</p>
-                                        <p>India</p>
-                                        <p>France</p>
-                                        <p>Germany</p>
-                                    </div>
-                                </li>
+            {/* navigation menu using Bootstrap */}
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-                                <li>Services</li>
-                                <li>Gallery</li>
-                                <li>About</li>
-                            </ul>
-                            <div className="d-flex d-lg-none" style={{ height: 200 }}>
-                                <div className="vr"></div>
-                            </div>
+            <div
+              className="navigation collapse navbar-collapse bg-transparent"
+              id="navbarNav"
+            >
+              <ul className="fw-normal px-0 mb-0 navbar-nav">
+                {NavLinks.map((nav, index) => {
+                  return (
+                    <li
+                      key={index}
+                      id={nav.id}
+                      className={nav.navTitle.submenu ? "hovermenu" : ""}
+                    >
+                      {typeof nav.navTitle === "string" ? (
+                        <span>{nav.navTitle}</span>
+                      ) : (
+                        <>
+                          <span>{nav.navTitle.title}</span>
+                          <div className="hovermenuitem">
+                            {nav.navTitle.submenu.map((submenu, index) => (
+                              <p key={index} id={submenu.id}>
+                                {submenu.title}
+                              </p>
+                            ))}
+                          </div>
+                        </>
+                      )}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="d-flex d-lg-none" style={{ height: 200 }}>
+                <div className="vr"></div>
+              </div>
 
-                            <div className="d-flex justify-content-between align-items-end gap-3">
-                                <button
-                                    type="button"
-                                    className="btn btn-dark"
-                                    data-bs-toggle="modal"
-                                    data-bs-target="#loginmodal"
-                                >
-                                    Login
-                                </button>
-                                <button
-                                    type="button"
-                                    className="btn btn-dark"
-                                    data-bs-toggle="modal"
-                                    data-bs-target="#registermodal"
-                                >
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
+              <div className="d-flex justify-content-between align-items-end gap-3">
+                <button
+                  type="button"
+                  className="btn btn-dark"
+                  data-bs-toggle="modal"
+                  data-bs-target="#loginmodal"
+                >
+                  Login
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-dark"
+                  data-bs-toggle="modal"
+                  data-bs-target="#registermodal"
+                >
+                  Register
+                </button>
+              </div>
+            </div>
+          </div>
+        </nav>
 
-                <section className="welcomesection text-center">
-                    <div className="welcome-content my-2">
-                        <h1>Welcome to Travels</h1>
-                        <h3>
-                            Visit <span id="placename"></span>
-                        </h3>
+        <section className="welcomesection text-center">
+          <div className="welcome-content my-2">
+            <h1>Welcome to Travels</h1>
+            <h3>
+              Visit <span id="placename"></span>
+            </h3>
 
-                        <div className="d-flex justify-content-center">
-                            <button className="btn btn-dark">Book Now</button>
-                        </div>
-                    </div>
-                </section>
-            </header>
-        </>
-    );
+            <div className="d-flex justify-content-center">
+              <button className="btn btn-dark">Book Now</button>
+            </div>
+          </div>
+        </section>
+      </header>
+    </>
+  );
 }
 
 export default Header;
